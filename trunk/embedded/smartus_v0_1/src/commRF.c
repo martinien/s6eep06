d171a53e1704482ba378cab558538bd1199c2b4a
pascalf79 <pascalf79@0cb0aa0f-1776-26e3-1f72-ae2ba34d9510>
2011-07-23 14:38:51 +0000

    Ajout de l'algorithme de synchronisation sur fanion (non-testé) mais le build est réussi. le debug est en cours

--- trunk/embedded/smartus_v0_1/src/commRF.c
+++ trunk/embedded/smartus_v0_1/src/commRF.c
@@ -1,12 +1,7 @@
 #include "commRF.h"
 #include "def.h"
 
-//Paramètres de la couche de liason de données
-#define FANION 0xE7
-#define NBRFANION 1
-#define ADRESSE 0xA0
-#define CONFIRMATION 0xAA
-#define ATTENTE_CONF 400	//milliseconde
+
 
 
 //==========================================================================================//
@@ -67,11 +62,95 @@ char rf_valider_confirmation(char *trame)
     
     //Validation
     if(trame[NBRFANION] == CONFIRMATION)
-	return 0;
+		return 0;
     return 1;
 }
 
 //Detection de trame complète
+void rf_detection_trame(char *RX, char *decalage, char *trame_complete, char *trame, char *cnt)
+{
+    if(*decalage != 0)
+    {
+	//Ranger les bits dans la trame afin d'obtenir une trame complète
+	trame[*cnt] = (*RX << (*decalage-1));
+	trame[*cnt-1] = (*RX >> 8-(*decalage-1)) & rf_decalage_masque(*decalage);
+	
+	//détection de la fin de la trame
+	if( *cnt > (NBRFANION+1) && trame[*cnt-1] == FANION)
+	{
+	    *trame_complete =1;
+	}
+    }
+    else
+    {
+	*decalage = rf_recherche_positif(RX);
+	if(*decalage != 0)
+	{
+	    trame[0] = (*RX << *decalage-1);
+	    ++cnt;
+	}
+    }
+    
+}
+
+//==========================================================================================//
+// Function: Recherche la possibilité d'avoir un fanion ou une partie			    //
+// Short description:	Define inputs and outputs					    //
+// In: 					octet de données de l'uart			    //
+// Out:					décalage du fanion = valeur retournée -1	    //
+//==========================================================================================//
+char rf_recherche_positif(char *RX)
+{
+    if((*RX & 0xE7) == 0xE7)
+	return 1;
+    if((*RX & 0x73) == 0x73)
+	return 2;
+    if((*RX & 0x39) == 0x39)
+	return 3;
+    if((*RX & 0x1C) == 0x1C)
+	return 4;
+    if((*RX & 0xE) == 0xE)
+	return 5;
+    if((*RX & 0x7) == 0x7)
+	return 6;
+    if((*RX & 0x3) == 0x3)
+	return 7;
+    if((*RX & 0x1) == 0x1)
+	return 8;
+
+    return 0;
+}
+
+char rf_decalage_masque(char *decalage)
+{
+    switch(*decalage-1)
+    {
+	case 1:
+	    return 0b00000001;
+	    break;
+	case 2:
+	    return 0b00000011;
+	    break;
+	case 3:
+	    return 0b00000111;
+	    break;
+	case 4:
+	    return 0b00001111;
+	    break;
+	case 5:
+	    return 0b00011111;
+	    break;
+	case 6:
+	    return 0b00111111;
+	    break;
+	case 7:
+	    return 0b01111111;
+	    break;
+	default:
+	    return 0;
+		break;
+    }
+}
 
 //CRC******************************************************************************
 
