55686715db792401ade68628a64bc4fddcb960d8
jfduval@aqra.ca <jfduval@aqra.ca@0cb0aa0f-1776-26e3-1f72-ae2ba34d9510>
2011-07-24 14:50:26 +0000

    Remise en état du code...

--- trunk/embedded/smartus_v0_1/src/def.h
+++ trunk/embedded/smartus_v0_1/src/def.h
@@ -20,8 +20,8 @@
 //Define DEBUG for terminal output
 //#define DEBUG
 #define USE_GLCD 
-#define BORNE
-//#define AUTO
+//#define BORNE
+#define AUTO
 //#define GPS_FEEDTHROUGH
 //test
 
@@ -104,11 +104,11 @@ void demo_numerisation(void);
 #define TRM_TX	1
 #define TRM_RX	0
 
-//Paramètres de la couche de liason de données
-#define FANION 0xE7
-#define NBRFANION 1
-#define ADRESSE 0xA0
-#define CONFIRMATION 0xAA
-#define ATTENTE_CONF 400	//milliseconde
+//Paramètres de la couche de liaison de données
+#define FANION 			0xE7
+#define NBRFANION 		1
+#define ADRESSE 		0xA0
+#define CONFIRMATION 	0xAA
+#define ATTENTE_CONF 	400		//millisecondes
 
 #endif
