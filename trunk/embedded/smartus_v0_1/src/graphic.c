11300769edf9679358904c8fe3a11387bbdb4227
lphilippe.b <lphilippe.b@0cb0aa0f-1776-26e3-1f72-ae2ba34d9510>
2011-08-01 14:17:38 +0000

    Leo graph optimises

--- trunk/embedded/smartus_v0_1/src/graphic.c
+++ trunk/embedded/smartus_v0_1/src/graphic.c
@@ -42,6 +42,7 @@ void GLCD_Rectangle(unsigned char x, unsigned char y, unsigned char b, unsigned 
 	}
 }
 
+/*
 //Devrait dessiner un cercle, mais fait plutôt des losanges... à travailler!
 void GLCD_Circle(unsigned char cx, unsigned char cy ,unsigned char radius)
 {
@@ -150,3 +151,4 @@ void GLCD_Line(unsigned int X1,unsigned int Y1,unsigned int X2,unsigned int Y2)
 	}
 }
 
+*/
